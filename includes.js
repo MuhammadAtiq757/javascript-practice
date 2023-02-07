65dec5fd97259bd156aa88411d9cc477d8891283
MuhammadAtiq <muhammadatiq757@gamil.com>
2023-02-07 20:06:38 +0600

    first commit

---
+++ includes.js
@@ -0,0 +1,3 @@
+var names = "Ariana grand is singing";
+var ariyana =names.includes("has");
+console.log(ariyana);
---
+++ subString.js
@@ -0,0 +1,3 @@
+var names = "tumi kemon acho jaan";
+var naam = names.substring(1, 6);
+console.log(naam);
